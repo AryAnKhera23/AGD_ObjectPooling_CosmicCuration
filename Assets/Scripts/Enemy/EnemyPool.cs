f8bc71d1764afa6e54bbb5a3c8c1e74e2fab513b
Aryan khera <aryan.khera2002@gmail.com>
2025-04-02 20:04:29 +0530

    Implemented Enemy Pool

---
+++ Assets/Scripts/Enemy/EnemyPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Enemy
+{
+    public class EnemyPool
+    {
+        private EnemyView enemyView;
+        private EnemyScriptableObject enemyScriptableObject;
+        private List<PooledEnemy> enemiesPool = new List<PooledEnemy>();
+
+        public class PooledEnemy
+        {
+            public EnemyController enemyConntroller;
+            public bool isUsed;
+        }
+
+        public EnemyPool(EnemyView enemyView, EnemyScriptableObject enemyScriptableObject)
+        {
+            this.enemyView = enemyView;
+            this.enemyScriptableObject = enemyScriptableObject;
+        }
+
+        public EnemyController GetEnemy()
+        {
+            if(enemiesPool.Count > 0)
+            {
+                PooledEnemy enemy = enemiesPool.Find(item => !item.isUsed);
+                if (enemy != null)
+                {
+                    enemy.isUsed = true;
+                    return enemy.enemyConntroller;
+                }
+            }
+            return CreateEnemy();
+        }
+
+        private EnemyController CreateEnemy()
+        {
+            PooledEnemy newEnemy = new PooledEnemy();
+            newEnemy.enemyConntroller = new EnemyController(enemyView, enemyScriptableObject.enemyData);
+            newEnemy.isUsed = true;
+            enemiesPool.Add(newEnemy);
+            return newEnemy.enemyConntroller;
+        }
+
+        public void ReturnToEnemyPool(EnemyController enemyController)
+        {
+            PooledEnemy enemy = enemiesPool.Find(item => item.Equals(enemyController));
+            if (enemy != null)
+            {
+                enemy.isUsed = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemyService.cs
+++ Assets/Scripts/Enemy/EnemyService.cs
@@ -7,8 +7,8 @@ namespace CosmicCuration.Enemy
     public class EnemyService
     {
         #region Dependencies
-        private EnemyView enemyPrefab;
         private EnemyScriptableObject enemyScriptableObject;
+        private EnemyPool enemyPool;
         #endregion
 
         #region Variables
@@ -20,8 +20,8 @@ namespace CosmicCuration.Enemy
         #region Initialization
         public EnemyService(EnemyView enemyPrefab, EnemyScriptableObject enemyScriptableObject)
         {
-            this.enemyPrefab = enemyPrefab;
             this.enemyScriptableObject = enemyScriptableObject;
+            enemyPool = new EnemyPool(enemyPrefab, enemyScriptableObject);
             InitializeVariables();
         }
 
@@ -59,9 +59,11 @@ namespace CosmicCuration.Enemy
 
         private void SpawnEnemyAtPosition(Vector2 spawnPosition, EnemyOrientation enemyOrientation)
         {
-            EnemyController spawnedEnemy = new EnemyController(enemyPrefab, enemyScriptableObject.enemyData);
+            EnemyController spawnedEnemy = enemyPool.GetEnemy();
             spawnedEnemy.Configure(spawnPosition, enemyOrientation);
         }
+
+        public void ReturnToEnemyPool(EnemyController enemyController) => enemyPool.ReturnToEnemyPool(enemyController);
 
         private Vector2 CalculateSpawnPosition(EnemyOrientation enemyOrientation)
         {
